49c9f021d8486b76a41832a7c773c2affca525de
Pablo Von Knoblauch Fontão <pablo-fontao@hotmail.com>
2021-11-07 17:59:54 -0300

    Conclusão da lista de exercícios Estruturas de Decisão

--- Atividade_1/estruturas_de_decisao.py
+++ Atividade_1/estruturas_de_decisao.py
@@ -1,3 +1,6 @@
+import math
+
+
 def teste_de_selecao_1():
     entry = str(input()).split()
 
@@ -151,8 +154,6 @@ def animal():
 
 
 def qual_triangulo():
-    # Considderando A e B como catetos e C como hipotenusa
-
     entry = str(input()).split()
     entry = [int(x) for x in entry]
     entry.sort()
@@ -180,8 +181,111 @@ def qual_triangulo():
 
 
 def avioes_de_papel():
-    pass
+    entry: list = input().split()
+    competitors: int = int(entry[0])
+    special_papers: int = int(entry[1])
+    papers_per_competitors: int = int(entry[2])
+
+    needed_paper = competitors * papers_per_competitors
+
+    if needed_paper > special_papers:
+        print("N")
+    else:
+        print("S")
+
+
+def sedex():
+    bowling_ball_diameter: int = int(input())
+    box_dimensions: list = input().split()
+    box_x: int = int(box_dimensions[0])
+    box_y: int = int(box_dimensions[1])
+    box_z: int = int(box_dimensions[2])
+
+    diameter_larger_than_dimensions: bool = bowling_ball_diameter <= box_x and bowling_ball_diameter <= box_y and bowling_ball_diameter <= box_z
+
+    if diameter_larger_than_dimensions:
+        print("S")
+    else:
+        print("N")
+
+
+def sedex_marciano():
+    order_from_marte: list = input().split()
+    box_x: int = int(order_from_marte[0])
+    box_y: int = int(order_from_marte[1])
+    box_z: int = int(order_from_marte[2])
+    ball_radius: int = int(order_from_marte[3])
+
+    box_diameter = math.sqrt((box_x ** 2) + (box_y ** 2) + (box_z ** 2))
+    box_fits_on_the_ball = (ball_radius * 2) >= box_diameter
+    
+    if box_fits_on_the_ball:
+        print("S")
+    else:
+        print("N")
+
+
+def vice_campeao():
+    classification = sorted(list(map(int, input().split())), reverse=True)
+    print(classification[1])
+
+
+def colcha():
+    mattress_dimensions = sorted(list(map(int, input().split())))
+    door_dimensions = sorted(list(map(int, input().split())))
+
+    matterss_amaller_than_door = mattress_dimensions[0] <= door_dimensions[0] and mattress_dimensions[1] <= door_dimensions[1]
+
+    if matterss_amaller_than_door:
+        print("S")
+    else:
+        print("N")
+
+
+def campeonato():
+    entry = input().split()
+    cormengo_wins = int(entry[0])
+    cormengo_draw = int(entry[1])
+    cormengo_goals = int(entry[2])
+    flaminthians_wins = int(entry[3])
+    flaminthians_draw = int(entry[4])
+    flaminthians_goals = int(entry[5])
+
+    cormengo_point = (cormengo_wins * 3) + cormengo_draw
+    flaminthians_points = (flaminthians_wins * 3) + flaminthians_draw
+
+    if flaminthians_points > cormengo_point:
+        print("F")
+    elif flaminthians_points < cormengo_point:
+        print("C")
+    else:
+        if flaminthians_goals > cormengo_goals:
+            print("F")
+        elif flaminthians_goals < cormengo_goals:
+            print("C")
+        else:
+            print("=")
+
+
+def corrida():
+    charrete_1 = input(). split()
+    charrete_2 = input(). split()
+    kilometers_per_hour_to_meters_per_second = 3.6
+    charrete_1_number = int(charrete_1[0])
+    charrete_1_distance = int(charrete_1[1])
+    charrete_1_velocity = int(charrete_1[2]) / kilometers_per_hour_to_meters_per_second
+    charrete_2_number = int(charrete_2[0])
+    charrete_2_distance = int(charrete_2[1])
+    charrete_2_velocity = int(charrete_2[2]) / kilometers_per_hour_to_meters_per_second
+
+    arrival_tome_to_charrete_1 = charrete_1_distance / charrete_1_velocity
+    arrival_tome_to_charrete_2 = charrete_2_distance / charrete_2_velocity
+
+    if arrival_tome_to_charrete_1 < arrival_tome_to_charrete_2:
+        print(charrete_1_number)
+    else:
+        print(charrete_2_number)
 
 
 if __name__ == "__main__":
-    avioes_de_papel()
+    corrida()
