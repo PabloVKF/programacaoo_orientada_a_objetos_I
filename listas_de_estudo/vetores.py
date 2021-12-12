42893a1056fb446985808fe1645b5fcd332ec05d
Pablo Von Knoblauch Fontão <pablo-fontao@hotmail.com>
2021-12-12 18:16:03 -0300

    Conclusão da lista de vetores

--- listas_de_estudo/vetores.py
+++ listas_de_estudo/vetores.py
@@ -127,7 +127,7 @@ def mergulho():
             break
 
 
-def circuito_bioquimico_digital(): # Incompleto
+def circuito_bioquimico_digital():
     while True:
         processing_points, mesurements_taken, stick_width = map(int, input().split())
         if not processing_points and not mesurements_taken and not stick_width:
@@ -135,11 +135,25 @@ def circuito_bioquimico_digital(): # Incompleto
 
         sample: list = []
         for number in range(mesurements_taken):
-            mesurement: int = int(input())
+            mesurement: list = list(map(int, input().split()))
             sample.append(mesurement)
 
+        number_of_stick: int = 0
+        for number in range(processing_points):
+            counter_of_ones: int = 0
+            for test in sample:
+                if test[number]:
+                    counter_of_ones += 1
+                    if counter_of_ones == stick_width:
+                        number_of_stick += 1
+                        counter_of_ones = 0
+                else:
+                    counter_of_ones = 0
 
-def detetive_watson(): # Incompleto
+        print(number_of_stick)
+
+
+def detetive_watson():
     while True:
         number_of_suspects: int = int(input())
         if not number_of_suspects:
@@ -150,5 +164,180 @@ def detetive_watson(): # Incompleto
         print(suspects[-2][0])
 
 
+def maquina_de_verificacao_automatica():
+    conector_1: list = list(map(int, input().split()))
+    conector_2: list = list(map(int, input().split()))
+
+    answer: str = "Y"
+    for number in range(len(conector_1)):
+        not_pluggable: bool = conector_1[number] == conector_2[number]
+        if not_pluggable:
+            answer = "N"
+            break
+
+    print(answer)
+
+
+def economia_brasileira():
+    number_of_participants: int = int(input())
+    answers: list = input().split()
+
+    majority_satisfied: bool = answers.count("0") > number_of_participants // 2
+    if majority_satisfied:
+        print("Y")
+    else:
+        print("N")
+
+
+def o_castelo_de_neve_de_sansa():
+    number_of_towers, number_of_peak = map(int, input().split())
+    sansa_towers: list = list(map(int, input().split()))
+
+    top_peak: int = 0
+    lower_peak: int = 0
+    for index, tower in enumerate(sansa_towers):
+        if index == 0:
+            continue
+        previous_tower: int = sansa_towers[index - 1]
+        if index == (len(sansa_towers) - 1):
+            next_tower: int = sansa_towers[-2]
+        else:
+            next_tower: int = sansa_towers[index + 1]
+        is_the_lower_peak: bool = tower == min(previous_tower, tower, next_tower)
+        is_the_top_peak: bool = tower == max(previous_tower, tower, next_tower)
+
+        if is_the_top_peak:
+            top_peak += 1
+        elif is_the_lower_peak:
+            lower_peak += 1
+
+    if top_peak == number_of_peak and lower_peak == number_of_peak:
+        print("beautiful")
+    else:
+        print("ugly")
+
+
+def pula_sapo():
+    jump_height, number_of_pipes = map(int, input().split())
+    pipe_sequence: list = list(map(int, input().split()))
+
+    game_result: str = 'YOU WIN'
+    for index, pipe in enumerate(pipe_sequence):
+        if index == 0:
+            continue
+        previous_pipe: int = pipe_sequence[index - 1]
+        cant_jump_over: bool = abs(pipe - previous_pipe) > jump_height
+
+        if cant_jump_over:
+            game_result = "GAME OVER"
+            break
+
+    print(game_result)
+
+
+def aeroporto():
+    counter_of_tests: int = 1
+    while True:
+        number_of_airports, number_of_flights = map(int, input().split())
+        if not number_of_airports and not number_of_flights:
+            break
+
+        airport_traffic: list = [[airport, 0] for airport in range(1, number_of_airports + 1)]
+        for flight_number in range(1, number_of_flights + 1):
+            departure, arrival = map(int, input().split())
+            airport_traffic[departure-1][1] += 1
+            airport_traffic[arrival-1][1] += 1
+
+        airport_traffic.sort(reverse=True, key=lambda x: x[1])
+        greater_traffic: int = airport_traffic[0][1]
+
+        print(f"Teste {counter_of_tests}")
+        for airport, traffic in airport_traffic:
+            if traffic == greater_traffic:
+                print(f"{airport} ", end="")
+            else:
+                break
+        print("\n")
+
+        counter_of_tests += 1
+
+
+def maratona():
+    number_of_posts, max_distance = map(int, input().split())
+    water_posts: list = list(map(int, input().split()))
+    end_of_marathon: int = 42195
+
+    water_posts.append(end_of_marathon)
+    marathon_result: str = "S"
+    for index, distance in enumerate(water_posts):
+        if index == 0:
+            continue
+
+        previous_post: int = water_posts[index - 1]
+        distance_between_posts: int = distance - previous_post
+
+        if distance_between_posts > max_distance:
+            marathon_result = "N"
+            break
+
+    print(marathon_result)
+
+
+def pulo_do_sapo():
+    number_of_stones, number_of_frogs = map(int, input().split())
+
+    occupation_stones: list = [0 for i in range(number_of_stones)]
+    for frog in range(number_of_frogs):
+        inicial_place, jump_width = map(int, input().split())
+        first_stone: int = inicial_place % jump_width
+        current_stone = first_stone
+
+        while True:
+            if not current_stone:
+                continue
+
+            try:
+                occupation_stones[current_stone - 1] = 1
+                current_stone += jump_width
+            except IndexError:
+                break
+
+        for number in range(len(occupation_stones) + 1):
+            stone_location: int = first_stone + (number * jump_width)
+            if not stone_location:
+                continue
+            try:
+                occupation_stones[stone_location - 1] = 1
+            except IndexError:
+                break
+
+    for stone in occupation_stones:
+        print(stone)
+
+
+def campo_minado():
+    number_of_cells: int = int(input())
+
+    board: list = []
+    for number in range(number_of_cells):
+        cell: int = int(input())
+        board.append(cell)
+
+    for index, cell in enumerate(board):
+        if index == 0:
+            previous_cell: int = index
+        else:
+            previous_cell: int = index - 1
+
+        if index == (len(board)-1):
+            next_cell: int = index
+        else:
+            next_cell: int = index + 1
+
+        print(sum(board[previous_cell:(next_cell + 1)]))
+
+
+
+
 if __name__ == "__main__":
-    detetive_watson()
+    campo_minado()
