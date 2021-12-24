4cc347f2f77f27b59cc0ca26aadb69b977d98f7f
Pablo Von Knoblauch Fontão <pablo-fontao@hotmail.com>
2021-12-24 17:16:46 -0300

    Conclusão da lista de strings

--- listas_de_estudo/strings.py
+++ listas_de_estudo/strings.py
@@ -166,5 +166,262 @@ def botas_perdidas():
             break
 
 
+def relogio_binario():
+    while True:
+        try:
+            hours, minutes = map(int, input().split(":"))
+
+            hour_divider: list = [8, 4, 2, 1]
+            hour_result: list = []
+            rest: int = hours
+            for divider in hour_divider:
+                if rest >= divider:
+                    hour_result.append("o")
+                    rest = rest % divider
+                else:
+                    hour_result.append(" ")
+
+            min_divider: list = [32, 16, 8, 4, 2, 1]
+            min_result: list = []
+            rest: int = minutes
+            for divider in min_divider:
+                if rest >= divider:
+                    min_result.append("o")
+                    rest = rest % divider
+                else:
+                    min_result.append(" ")
+
+            print(f" ____________________________________________")
+            print(f"|                                            |")
+            print(f"|    ____________________________________    |_")
+            print(f"|   |                                    |   |_)")
+            print(f"|   |   8         4         2         1  |   |")
+            print(f"|   |                                    |   |")
+            print(f"|   |   {hour_result[0]}         {hour_result[1]}         {hour_result[2]}         {hour_result[3]}  |   |")
+            print(f"|   |                                    |   |")
+            print(f"|   |                                    |   |")
+            print(f"|   |   {min_result[0]}     {min_result[1]}     {min_result[2]}     {min_result[3]}     {min_result[4]}     {min_result[5]}  |   |")
+            print(f"|   |                                    |   |")
+            print(f"|   |   32    16    8     4     2     1  |   |_")
+            print(f"|   |____________________________________|   |_)")
+            print(f"|                                            |")
+            print(f"|____________________________________________|")
+            print()
+
+        except EOFError:
+            break
+
+
+def deli_deli():
+    irregular_words_number, total_words = map(int, input().split())
+    word_ends_with: list = ["o", "s", "ch", "sh", "x"]
+    vowels: list = ["a", "e", "i", "o", "u"]
+    irregular_words: list = []
+    plu_irregular_words: list = []
+
+    for number in range(irregular_words_number):
+        irregular, plural = input().split()
+        irregular_words.append(irregular)
+        plu_irregular_words.append(plural)
+
+    for number in range(total_words):
+        word: str = input()
+        if word in irregular_words:
+            index: int = irregular_words.index(word)
+            word = plu_irregular_words[index]
+            print(word)
+        elif (word[-1] == "y") and (word[-2].lower() not in vowels):
+            word = word[:-1] + "ies"
+            print(word)
+        elif word[-1] in word_ends_with:
+            word += "es"
+            print(word)
+        elif word[-2:] in word_ends_with:
+            word += "es"
+            print(word)
+        else:
+            word += "s"
+            print(word)
+
+
+def perdido_em_marte():
+    unnecessary_entry: str = input()
+    hex_letters: list = input().split()
+
+    for hexadecimal in hex_letters:
+        bytes_letter: bytes = bytes.fromhex(hexadecimal)
+        ascii_letter: str = bytes_letter.decode("ASCII")
+        print(ascii_letter, end='')
+
+    print()
+
+
+def nome_no_formulario():
+    texte: str = input()
+    limit: int = 80
+
+    if len(texte) > limit:
+        print("NO")
+    else:
+        print("YES")
+
+
+def banco_imobiliario():
+    money, number_of_transations = map(int, input().split())
+    players: dict = {
+        "D": money,
+        "E": money,
+        "F": money
+    }
+
+    for number in range(number_of_transations):
+        transations = input().split()
+        type_tran: str = transations[0]
+        player: str = transations[1]
+
+        if type_tran == "C":
+            value: int = int(transations[2])
+            players[player] -= value
+        elif type_tran == "V":
+            value: int = int(transations[2])
+            players[player] += value
+        elif type_tran == "A":
+            player_2: str = transations[2]
+            value: int = int(transations[3])
+            players[player] += value
+            players[player_2] -= value
+
+    print(f"{players['D']} {players['E']} {players['F']}")
+
+
+def lingua_do_P():
+    coded_sentence: str = input()
+    decoded_sentence: str = ""
+
+    contador: int = 0
+    while contador < len(coded_sentence):
+        letter = coded_sentence[contador]
+        if letter == "p":
+            decoded_sentence += coded_sentence[contador + 1]
+            contador += 2
+        else:
+            decoded_sentence += coded_sentence[contador]
+            contador += 1
+
+    print(decoded_sentence)
+
+
+def letras():
+    letter: str = input()
+    text: list = input().split()
+    number_of_words: int = len(text)
+    occurrences: int = 0
+
+    for word in text:
+        if letter in word:
+            occurrences += 1
+
+    percentage_of_occurrrence: float = (occurrences / number_of_words) * 100
+    print(f"{percentage_of_occurrrence:.1f}")
+
+
+def nova_senha_ra():
+    number_of_passwords: int = int(input())
+    logic: list = [
+        ("GQaku", "0"),
+        ("ISblv", "1"),
+        ("EOYcmw", "2"),
+        ("FPZdnx", "3"),
+        ("JTeoy", "4"),
+        ("DNXfpz", "5"),
+        ("AKUgq", "6"),
+        ("CMWhr", "7"),
+        ("BLVis", "8"),
+        ("HRjt", "9")
+    ]
+
+    for number in range(number_of_passwords):
+        old_password: str = input()
+        new_password: str = ''
+
+        for letter in old_password:
+
+            for letter_set, number in logic:
+                if letter in letter_set:
+                    new_password += number
+                    break
+
+            if len(new_password) == 12:
+                break
+        print(new_password)
+
+
+def evitando_chuva():
+    number_of_days: int = int(input())
+    umbrella_home: int = 0
+    umbrella_work: int = 0
+    home_stock: int = 0
+    work_stock: int = 0
+
+    for number in range(number_of_days):
+        home, work = input().split()
+        if home == "chuva":
+            if home_stock:
+                home_stock -= 1
+                work_stock += 1
+            else:
+                umbrella_home += 1
+                work_stock += 1
+        if work == "chuva":
+            if work_stock:
+                work_stock -= 1
+                home_stock += 1
+            else:
+                umbrella_work += 1
+                home_stock += 1
+
+    print(umbrella_home, umbrella_work)
+
+
+def lexico():
+    word_1: str = input()
+    word_2: str = input()
+    ordered_words: list = sorted([word_1, word_2])
+
+    for word in ordered_words:
+        print(word)
+
+
+def criptotexto():
+    number_of_tests: int = int(input())
+
+    for number in range(number_of_tests):
+        encrypted_word: list = list(input())
+        encrypted_word.reverse()
+        decrypted_word: str = ""
+
+        for letter in encrypted_word:
+            if letter.islower():
+                decrypted_word += letter
+
+        print(decrypted_word)
+
+
+def frase_binaria():
+    while True:
+        try:
+            number_of_tests: int = int(input())
+            word: str = ""
+
+            for number in range(number_of_tests):
+                decimal: int = int(input(), base=2)
+                ascii_letter: str = chr(decimal)
+                word += ascii_letter
+
+            print(word)
+        except EOFError:
+            break
+
+
 if __name__ == "__main__":
-    botas_perdidas()
+    frase_binaria()
